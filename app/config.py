a7df5bb042d45a237ffcfbc3a2ce455061deaff6
S3b2 <111647190+Shaina-08@users.noreply.github.com>
2024-12-09 22:03:29 +0530

    change

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ import os
 
 load_dotenv()
 
+ALLOWED_HOSTS = "http://localhost:3000"
 class Settings:
     database_hostname: str = os.getenv("DATABASE_HOSTNAME")
     database_port: int = os.getenv("DATABASE_PORT")
@@ -12,5 +13,7 @@ class Settings:
     secret_key: str = os.getenv("SECRET_KEY")
     algorithm: str = os.getenv("ALGORITHM")
     access_token_expire_minutes: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-
+    ALLOWED_HOSTS: list =ALLOWED_HOSTS.split(",")
+    class Config:
+        env_file = ".env"
 settings = Settings()
