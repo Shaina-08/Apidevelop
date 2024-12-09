93f38f048a09548ba8fef982d76fb9d11c0611f2
S3b2 <111647190+Shaina-08@users.noreply.github.com>
2024-12-09 22:18:47 +0530

    change

--- app/database.py
+++ app/database.py
@@ -5,11 +5,14 @@ from .config import settings
 
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+print(f"SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+print(f"SQLALCHEMY_DATABASE_URL: {engine}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 print(f"Database Port: {settings.database_port}")  # Should print 2224
-print(f"SQLALCHEMY_DATABASE_URL: {SQLALCHEMY_DATABASE_URL}")  # Should print postgresql://postgres:Shaina05@@localhost:2224/fastapi
+
+  # Should print postgresql://postgres:Shaina05@@localhost:2224/fastapi
 def get_db():
     db = SessionLocal()
     try:
