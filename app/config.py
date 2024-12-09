abf9b1215927304693d71946f57429ee527b3aff
S3b2 <111647190+Shaina-08@users.noreply.github.com>
2024-12-09 21:58:57 +0530

    change

--- app/config.py
+++ app/config.py
@@ -1,22 +1,14 @@
-from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
 
-class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_name: str
-    database_username: str
-    database_password: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-    ALLOWED_HOSTS: list = []
-    
+load_dotenv()
 
-    class Config:
-        env_file = ".env"
-        # env_file_encoding = "utf-8"
-    
-    
-
-settings = Settings()
-
+class Settings:
+    database_hostname: str = os.getenv("DATABASE_HOSTNAME")
+    database_port: int = os.getenv("DATABASE_PORT")
+    database_name: str = os.getenv("DATABASE_NAME")
+    database_username: str = os.getenv("DATABASE_USERNAME")
+    database_password: str = os.getenv("DATABASE_PASSWORD")
+    secret_key: str = os.getenv("SECRET_KEY")
+    algorithm: str = os.getenv("ALGORITHM")
+    access_token_expire_minutes: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
