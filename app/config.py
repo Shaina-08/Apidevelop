f0d59cec26620bb55add08a98beedac52d9c66e1
S3b2 <111647190+Shaina-08@users.noreply.github.com>
2024-12-09 22:00:25 +0530

    change

--- app/config.py
+++ app/config.py
@@ -12,3 +12,5 @@ class Settings:
     secret_key: str = os.getenv("SECRET_KEY")
     algorithm: str = os.getenv("ALGORITHM")
     access_token_expire_minutes: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+settings = Settings()
